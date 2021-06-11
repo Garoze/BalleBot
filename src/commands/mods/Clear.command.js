89b571fcd3c71d2f01042e39e0be72212cb792dc
Garoze <marcelogaroze@hotmail.com>
2021-06-11 20:52:43 -0300

    refactor(clearcommand): added MessageEmbed and self delete message to Clean.command.js
    I've refactored the whole code for this command.

--- src/commands/mods/Clear.command.js
+++ src/commands/mods/Clear.command.js
@@ -1,29 +1,40 @@
+import { MessageEmbed } from 'discord.js';
+
+const guildID = '634206579327107112';
+
 export default {
   name: 'Clear',
   aliases: ['cls', 'faxina'],
   description: 'Clear a certain number of messages from a text channel.',
   permissions: ['MANAGE_MESSAGES'],
-  run: async ({ message, args }) => {
-    if (args[0]) {
-      if (isNaN(args[0]))
-        return message.reply('Você precisa informar um numero!');
-
-      if (args[0] >= 1 && args[0] <= 100) {
-        message.delete();
-        await message.channel.messages
-          .fetch({ limit: args[0] })
-          .then((messages) => {
-            message.channel.bulkDelete(messages, true);
-          });
-      } else {
-        return message.reply(
-          'Você precisa informar o numero de mensagens a serem deletadas, entre 1 e 100.'
-        );
-      }
-    } else {
+  run: async ({ client, message, args }) => {
+    if (!args[0] || isNaN(args[0]))
       return message.reply(
-        'Você precisa informar o numero de mensagens a serem deletadas!'
+        'You need to inform the message number to be deleted.'
       );
+
+    if (args[0] >= 1 && args[0] <= 100) {
+      await message.delete();
+      await message.channel.messages
+        .fetch({ limit: args[0] })
+        .then((msg) => message.channel.bulkDelete(msg, true));
+
+      const embed = new MessageEmbed()
+        .setColor(client.guilds.cache.get(guildID).me.displayHexColor)
+        .setTitle('🧹 Cleaning up...')
+        .setAuthor(message.author.tag, message.author.avatarURL())
+        .setDescription(
+          `Good now everything is clean.\nA total of **${args[0]}** messages were deleted from this channel.`
+        )
+        .setThumbnail(client.user.avatarURL())
+        .setTimestamp()
+        .setFooter(`Command by: ${message.member.user.tag}`);
+
+      message.channel
+        .send(embed)
+        .then((msg) => setTimeout(() => msg.delete(), 5000));
+    } else {
+      return message.reply('Please inform a number between 1 and 100.');
     }
   },
 };
