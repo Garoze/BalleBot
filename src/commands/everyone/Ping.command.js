14380530b044d8732882e37d856796cc69c989f3
Garoze <marcelogaroze@hotmail.com>
2021-06-11 20:22:20 -0300

    refactor(pingcommand): added MessageEmbed to Ping.command.js

--- src/commands/everyone/Ping.command.js
+++ src/commands/everyone/Ping.command.js
@@ -1,9 +1,27 @@
+import { MessageEmbed } from 'discord.js';
+
+const guildID = '634206579327107112';
+
 export default {
   name: 'Ping',
-  description: '',
-  aliases: ['pg'],
-  permissions: [],
-  run: ({ message }) => {
-    message.reply('Pong!');
+  description: 'Checks the ping.',
+  aliases: ['latency'],
+  permissions: ['SEND_MESSAGE'],
+  run: ({ client, message }) => {
+    const embed = new MessageEmbed()
+      .setColor(client.guilds.cache.get(guildID).me.displayHexColor)
+      .setTitle('Pong! 🏓')
+      .setAuthor(message.author.tag, message.author.avatarURL())
+      .setDescription(
+        `**Latency** is \`${
+          Date.now() - message.createdTimestamp
+        }\` ms. **API Latency** is \`${Math.round(client.ws.ping)}\` ms`
+      )
+      .setThumbnail(client.user.avatarURL())
+      .setTimestamp()
+      .setFooter(`Command by: ${message.member.user.tag}`);
+
+    message.delete();
+    message.channel.send(embed);
   },
 };
